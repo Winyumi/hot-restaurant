51b5662efdc02bdaeead1d8156b22710d7ec3396
shaili shah <sshaili2912@gmail.com>
2019-11-28 21:40:00 -0500

    change in route

--- server.js
+++ server.js
@@ -1,28 +1,44 @@
 // Dependencies
 // =============================================================
-const Express = require('express');
+const express = require('express');
 
 // Sets up the Express App
 // =============================================================
-const app = Express();
+const app = express();
 const PORT = process.env.PORT || 3000;
 
 // Sets up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+// var tables = [
+// {
+//     TableID : ID,
+//     Name : name ,
+//     Email: email,
+//     Phone : phoneNum
+
+// }
+// ]
+
 // Routes
 // =============================================================
+app.post("/reservationView",function(req,res){
+    var newtables = req.body;
+    tables.push(newtables);
+    alert("Reservation confirm")
+})
+
 app.get("/", function(req, res) {
-    res.sendFile(path.join(__dirname, "index.html"));
+    res.sendFile(__dirname + "/public/index.html");
   });
 
-app.get("/tables", function(req,res){
-    res.sendFile(path.join(__dirname, "reservation_add.html"));
+app.get("/reservation", function(req,res){
+    res.sendFile(__dirname + "/public/tables.html");
 });
 
 app.get("/reservationView", function(req,res){
-    res.sendFile(path.join(__dirname, "reservation_view.html"));
+    res.sendFile(__dirname + "/public/reservation_view.html");
 });
 // Starts the server to begin listening
 // =============================================================
