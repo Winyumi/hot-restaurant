55c08149ecdfef65f350735d220c3ea1cfae13b3
Sal Tamay <sal.tamay@outlook.com>
2019-11-28 21:35:44 -0500

    update routes

--- server.js
+++ server.js
@@ -1,31 +1,34 @@
 // Dependencies
 // =============================================================
-const Express = require('express');
+const express = require('express');
 
 // Sets up the Express App
 // =============================================================
-const app = Express();
+const app = express();
 const PORT = process.env.PORT || 3000;
 
 // Sets up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+// Serve Static files
+app.use(express.static('public'));
+
 
 // Routes
 // =============================================================
-app.get("/", function(req, res) {
-    res.sendFile(path.join(__dirname, "index.html"));
-  });
-
-app.get("/tables", function(req,res){
-    res.sendFile(path.join(__dirname, "reservation_add.html"));
+app.get("/", function (req, res) {
+  res.sendFile(__dirname + "/public/index.html");
 });
 
-app.get("/reservationView", function(req,res){
-    res.sendFile(path.join(__dirname, "reservation_view.html"));
+app.get("/tables", function (req, res) {
+  res.sendFile(__dirname + "/public/tables.html");
+});
+
+app.get("/reservation", function (req, res) {
+  res.sendFile(__dirname + "/public/reservation_add.html");
 });
 // Starts the server to begin listening
 // =============================================================
-app.listen(PORT, function() {
+app.listen(PORT, function () {
   console.log("App listening on PORT " + PORT);
 });
