e590686eca37107fddcb35f3c6116a7d28551c86
Sal Tamay <sal.tamay@outlook.com>
2019-11-28 21:43:32 -0500

    REsolve merge conflict

--- server.js
+++ server.js
@@ -20,15 +20,15 @@ app.use(Express.json());
 //================================================================
 var tables = [
   {
-      TableID : ID,
-      Name : name ,
-      Email: email,
-      Phone : phoneNum
-  
-  }
-  ]
+    TableID: ID,
+    Name: name,
+    Email: email,
+    Phone: phoneNum
 
-app.post("/reservationView",function(req,res){
+  }
+]
+
+app.post("/reservationView", function (req, res) {
   var newtables = req.body;
   tables.push(newtables);
   alert("Reservation confirm");
@@ -47,47 +47,26 @@ app.post("/reservationView",function(req,res){
 
 // Routes
 // =============================================================
-app.post("/reservationView",function(req,res){
-    var newtables = req.body;
-    tables.push(newtables);
-    alert("Reservation confirm")
+app.post("/reservationView", function (req, res) {
+  var newtables = req.body;
+  tables.push(newtables);
+  alert("Reservation confirm")
 })
 
-app.get("/", function(req, res) {
-<<<<<<< HEAD
-    res.sendFile(path.join(__dirname, "public/index.html"));
-  });
-
-app.get("/tables", function(req,res){
-    res.sendFile(path.join(__dirname, "public/reservation_add.html"));
+app.get("/", function (req, res) {
+  res.sendFile(__dirname + "/public/index.html");
 });
 
-app.get("/reservationView", function(req,res){
-    res.sendFile(path.join(__dirname, "public/reservation_view.html"));
+app.get("/reservation", function (req, res) {
+  res.sendFile(__dirname + "/public/tables.html");
 });
 
-app.post("/tables/add", function(req, res) {
-  // req.body hosts is equal to the JSON post sent from the user
-  // This works because of our body parsing middleware
-  let newReserveration = req.body;
-  
-  // Using a RegEx Pattern to remove spaces from newCharacter
-  // You can read more about RegEx Patterns later https://www.regexbuddy.com/regex.html
-  
-=======
-    res.sendFile(__dirname + "/public/index.html");
-  });
+app.get("/reservationView", function (req, res) {
+  res.sendFile(__dirname + "/public/reservation_view.html");
 
-app.get("/reservation", function(req,res){
-    res.sendFile(__dirname + "/public/tables.html");
-});
-
-app.get("/reservationView", function(req,res){
-    res.sendFile(__dirname + "/public/reservation_view.html");
->>>>>>> 51b5662efdc02bdaeead1d8156b22710d7ec3396
 });
 // Starts the server to begin listening
 // =============================================================
-app.listen(PORT, function() {
+app.listen(PORT, function () {
   console.log("App listening on PORT " + PORT);
 });
